220895ad55466beabe93ac73f3511d0b4caac5e8
Vincenzo Maffione <v.maffione@gmail.com>
2017-02-16 15:03:13 +0100

    24: skip factorials

--- project-euler/24/24.cpp
+++ project-euler/24/24.cpp
@@ -5,12 +5,17 @@
 
 using namespace std;
 
+#define DIGITS          10
+#define SELECT_PERM     1000000
+
+vector<unsigned long> factorials;
+
 static void
 generate_perm(int n, stack<char>& st, vector<bool>& marked, int& cnt)
 {
     if (n <= 0) {
         cnt ++;
-        if (cnt == 1000000) {
+        if (cnt == SELECT_PERM) {
             stack<char> t = st;
             string s;
 
@@ -24,28 +29,39 @@ generate_perm(int n, stack<char>& st, vector<bool>& marked, int& cnt)
         return;
     }
 
-    for (unsigned i = 0; i < marked.size(); i ++) {
+    for (unsigned i = 0; i < marked.size() && cnt < SELECT_PERM; i ++) {
         if (!marked[i]) {
-            marked[i] = true;
-            st.push(i);
-            generate_perm(n-1, st, marked, cnt);
-            st.pop();
-            marked[i] = false;
+            if (cnt + factorials[n-1] < SELECT_PERM) {
+                /* Just skip them */
+                cnt += factorials[n-1];
+            } else {
+                marked[i] = true;
+                st.push(i);
+                generate_perm(n-1, st, marked, cnt);
+                st.pop();
+                marked[i] = false;
+            }
         }
     }
 }
 
 int main()
 {
-    vector<bool> marked(10);
+    vector<bool> marked(DIGITS);
     int perm_cnt = 0;
     stack<char> st;
 
-    for (unsigned i = 0; i < marked.size(); i ++) {
+    /* Precompute factorials */
+    factorials.push_back(1 /* 0! */);
+    for (unsigned i = 1; i <= DIGITS; i ++) {
+        factorials.push_back(factorials[i-1] * i);
+    }
+
+    for (unsigned i = 0; i < DIGITS; i ++) {
         marked[i] = false;
     }
 
-    generate_perm(10, st, marked, perm_cnt);
+    generate_perm(DIGITS, st, marked, perm_cnt);
 
     return 0;
 }
