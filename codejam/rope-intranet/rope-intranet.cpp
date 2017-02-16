4675fc44bb137c988cdd3e2193b4aca29eceff83
Vincenzo Maffione <v.maffione@gmail.com>
2017-02-16 19:23:11 +0100

    rope-intranet: comments

--- codejam/rope-intranet/rope-intranet.cpp
+++ codejam/rope-intranet/rope-intranet.cpp
@@ -10,6 +10,7 @@ static void
 print_pairs(const vector<pair<unsigned, unsigned> >& pairs,
             bool left)
 {
+#if 0
         if (left) {
             cout << "Lpairs: " << endl;
         } else {
@@ -23,6 +24,7 @@ print_pairs(const vector<pair<unsigned, unsigned> >& pairs,
                 cout << "    " << j->second << ", " << j->first << endl;
             }
         }
+#endif 0
 }
 
 int main()
@@ -38,6 +40,9 @@ int main()
 
         cin >> n;
 
+         /* Push the couples into two vectors amd sort them: lpairs keeps them
+          * ordered w.r.t. the first coordinate, rpairs keeps
+          * them ordered w.r.t. the second coordinate. */
         for (unsigned j = 0; j < n; j ++) {
             unsigned a, b;
 
@@ -49,20 +54,25 @@ int main()
         sort(lpairs.begin(), lpairs.end());
         sort(rpairs.begin(), rpairs.end());
 
-
         print_pairs(lpairs, true);
         print_pairs(rpairs, false);
 
+         /* Scan the sets in parallel (in ascending order, since
+          * this is a set). */
         auto i = lpairs.begin();
         auto j = rpairs.begin();
 
         for (; i != lpairs.end();) {
+            /* If the order of the couples so far is the same, keep
+             * going, it means so far there are no intersections. */
             if (i->first == j->second /* or i->second == j->first */) {
                 j ++;
                 i ++;
                 continue;
             }
 
+            /* Otherwise we lineary-search where the i-th couple is
+             * in rpairs. */
             auto k = j + 1;
 
             while (k != rpairs.end() && k->first != i->second) {
@@ -70,8 +80,11 @@ int main()
             }
             assert(k != rpairs.end());
 
+            /* The number of intersections caused by k is the difference
+             * between the position in rpairs and the one in lpairs. */
             total_moves += k - j;
 
+            /* We move k right before j. */
             pair<unsigned, unsigned> x = *k;
             rpairs.erase(k);
             rpairs.insert(j, x);
